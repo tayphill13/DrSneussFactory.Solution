2919a725f0bba89ecbee7cafa8e9c2e567697f50
Taylor Phillips <taylorphillips133@gmail.com>
2020-08-07 10:58:09 -0700

    add the Edit functionality to the MachinesController

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -29,7 +29,31 @@ namespace Factory.Controllers
     }
     public ActionResult Edit(int id)
     {
-      
+      var thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId ==id);
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+      return View(thisMachine);
     }
+
+    [HttpPost]
+    public ActionResult Edit(Machine machine, int EngineerId)
+    {
+      var joinConfirm = _db.EngineerMachine.FirstOrDefault(join => join.MachineId == machine.MachineId && join.EngineerId == EngineerId);
+
+      if(joinConfirm != null)
+      {
+        _db.Entry(machine).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Details", new { id = machine.MachineId});
+      }
+      if (EngineerId != 0)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId });
+      }
+      _db.Entry(machine).State =EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Details", new {id = machine.MachineId});
+    }
+
+    
   }
 }
